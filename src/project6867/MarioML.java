6d52ed3a2f8b49c565b77730bbecad0eb80730a6
Erek Speed <melink14@gmail.com>
2011-12-04 16:40:03 -0500

    Added RF code.  Added a bunch of data to svmreports

--- src/project6867/MarioML.java
+++ src/project6867/MarioML.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,6 +18,7 @@ import libsvm.LibSVM;
 import net.sf.javaml.classification.Classifier;
 import net.sf.javaml.classification.evaluation.CrossValidation;
 import net.sf.javaml.classification.evaluation.PerformanceMeasure;
+import net.sf.javaml.classification.tree.RandomForest;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.DefaultDataset;
 import net.sf.javaml.core.Instance;
@@ -24,7 +27,12 @@ import net.sf.javaml.core.SparseInstance;
 public class MarioML {
 
 	private static final int FEATURE_SIZE = 4695;
-	private static Dataset data;	
+	private static final int PER_FEATURE = 1000;
+	private static final int TREES = 500;
+	private static Dataset data;
+	private static HashSet<Integer> features = new HashSet<Integer>(500);
+	private static String output;
+	private static Classifier best;
 
 
 	/**
@@ -44,26 +52,26 @@ public class MarioML {
 		filenames.add("eneimesblocksgaps.data");
 		filenames.add("eneimesblocks.data");
 		
-		int datasize = 100000;
-		
+		int datasize = 5000;
+	
 		float gamma = .5f;
 		float C = 10f;
 		String kernel = "rbf";
 		
-		String output = "svm" + kernel + "g" + gamma + "c" + C + "ds" + datasize +".out";
+		//output = "reduced10svm" + kernel + "g" + gamma + "c" + C + "ds" + datasize +".out";
+		output = "randomforest" + "trees" + TREES + ".out";
+		//MarioML.loadFeatureSpace("forward10percent2500mixedpoints.data");
+		Classifier clf = marioml.runRF(filenames);
 		
-		
-		Classifier clf = marioml.runSVM(filenames, datasize, C, gamma, kernel);
-		
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(output));
-		
-		oos.writeObject(clf);
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(output));
+//		
+//		oos.writeObject(clf);
 
 	}
 	
 	
 	private Classifier runSVM(Collection<String> filenames, int datasize, float c,
-			float gamma, String kernel) {
+			float gamma, String kernel) throws FileNotFoundException, IOException {
 		
 		for(String str : filenames) {
 			System.out.println("load " + str);
@@ -79,14 +87,17 @@ public class MarioML {
 		svm.getParameters().C = c;
 		svm.getParameters().kernel_type = libsvm.svm_parameter.RBF;
 		svm.getParameters().gamma = gamma;
-		svm.getParameters().cache_size = 1000;
+		svm.getParameters().cache_size = 2000;
 		
 		svm.buildClassifier(data);
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(output));
+		oos.writeObject(svm);
+		oos.close();
 		
 		System.out.println(System.currentTimeMillis() - start);
 		
 		start = System.currentTimeMillis();
-		Map<Object, PerformanceMeasure> p = new CrossValidation(svm).crossValidation(data, 5);
+		Map<Object, PerformanceMeasure> p = new CrossValidation(svm).crossValidation(data, 2);
 		System.out.println(System.currentTimeMillis() - start);
 		
 		for(Object o:p.keySet()) {
@@ -100,6 +111,34 @@ public class MarioML {
 		return svm;
 		
 	}
+	
+	private Classifier runRF(Collection<String> filenames) throws FileNotFoundException, IOException {
+		
+		for(String str : filenames) {
+			System.out.println("load " + str);
+			MarioML.loadFile(str);
+		}
+		
+//		//Sampling s = Sampling.StratifiedSubsampling;
+//		Pair<Dataset, Dataset> datas = s.sample(data, datasize);
+//		System.out.println("Done Sampling");
+		
+		long start = System.currentTimeMillis();
+		RandomForest rf = new RandomForest(TREES, true, FEATURE_SIZE, new Random());
+		
+		rf.buildClassifier(data);
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(output));
+		oos.writeObject(rf);
+		oos.close();
+		
+		System.out.println(System.currentTimeMillis() - start);
+		
+		System.out.println(rf.getOutOfBagErrorEstimate());
+		
+		return rf;
+		
+	}
+
 
 
 	/*
@@ -123,10 +162,14 @@ public class MarioML {
 					for(String index : m.group(2).split(", ")){
 						if(index.equals(""))
 							break;
+						Integer tInd = Integer.parseInt(index);
+						if(!features.contains(tInd))
+							continue;
 						feature_vector.put(Integer.parseInt(index), 1.0);
 					}
+					//System.out.println(feature_vector.size());
 					data.add(feature_vector);
-					if(++count >= 25000)
+					if(++count >= PER_FEATURE)
 						break;
 				}
 			}
@@ -134,6 +177,20 @@ public class MarioML {
 			e.printStackTrace();
 		}
 
+	}
+	
+	private static void loadFeatureSpace(String filename) {
+		try {
+			BufferedReader r = new BufferedReader(new FileReader(filename));
+			String indices = r.readLine();
+			for(String index : indices.split(", ")) {
+				features.add(Integer.parseInt(index));
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
